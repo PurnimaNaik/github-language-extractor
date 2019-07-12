545cceb4fe5041ef71120594a9a13f32ccd36e53
PurnimaNaik <naikpurnima9@gmail.com>
2019-07-12 16:41:18 -0400

    Search icon added

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Text, View, StyleSheet, TextInput, Dimensions} from 'react-native';
-// import ProgressCircleBase from './ProgressCircleBase';
+import { Text, View, StyleSheet, TextInput, Dimensions, Image } from 'react-native';
+// import ProgressCircleBase from './ProgressCircleBase';searchIcon.png
 
 const SearchBar = ({}) => {
   return (
     <View style={styles.container}>
-    <View style={styles.searchBox}>
-    <TextInput style={styles.textBox} placeholder="Enter username"></TextInput>
-    </View>  
+      <View style={styles.searchBox}>
+      <Image  style={styles.searchIcon} source={require('../Images/searchIcon.png')} />
+        <TextInput placeholder="Enter username">
+        </TextInput>
+      </View>
     </View>
   );
 };
@@ -18,25 +20,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   searchBox: {
+    flexDirection: 'row',
     backgroundColor: 'white',
-    justifyContent: 'center',
-    // alignItems: 'left',
+    // justifyContent: 'center',
+    alignItems: 'center',
     height: 30,
-    width: Dimensions.get('window').width-50,
+    width: Dimensions.get('window').width - 50,
     borderRadius: 45,
     shadowColor: 'black',
     shadowOffset: { width: 0, height: 0 },
     shadowOpacity: 0.1,
     shadowRadius: 3,
-    marginTop:25,
-    
+    marginTop: 25,
   },
-  textBox:{
-    paddingLeft:10,
-  },
-  categoryOne: {
-    opacity: 1,
-    position: 'absolute',
+  searchIcon: {
+    height: 23,
+    width: 23,
+    marginLeft: 10,
+    marginRight: 5,
+    justifyContent: 'center',
   },
   categoryTwo: {
     opacity: 0.5,
