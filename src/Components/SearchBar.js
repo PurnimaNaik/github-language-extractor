b68b52de6a6597c15fb7dfd2a1455e28cb542afb
PurnimaNaik <naikpurnima9@gmail.com>
2019-07-13 22:19:33 -0400

    Extracting deeper languages

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -15,13 +15,14 @@ class SearchBar extends React.Component {
     this.state = {
       languageCollection: [],
       languageURlCollection: [],
-      response:null,
+      deepLanguageCollection: [],
+      response: null,
+      repoLanguageResponse: null,
     };
   }
 
   getUserRepos = username => {
     console.log(username);
-
     try {
       fetch('https://api.github.com/users/' + username + '/repos', {
         method: 'GET',
@@ -33,19 +34,21 @@ class SearchBar extends React.Component {
         .then(response => response.json())
         .then(responseJson => {
           console.log(responseJson);
-          this.setState({
-            response:responseJson
-          },()=>{
-            this.getLanguagePool();
-          });
-         
+          this.setState(
+            {
+              response: responseJson,
+            },
+            () => {
+              this.getShallowLanguagePool();
+            }
+          );
         });
     } catch (error) {
       console.log(error);
     }
   };
 
-  getLanguagePool = () => {
+  getShallowLanguagePool = () => {
     var langauageArray = [];
     var languageURLArray = [];
     for (i = 0; i < this.state.response.length; i++) {
@@ -53,14 +56,75 @@ class SearchBar extends React.Component {
       languageURLArray.push(this.state.response[i].languages_url);
     }
 
-    this.setState({
-      languageCollection: langauageArray,
-      languageURlCollection: languageURLArray,
-    },()=>{
-      console.log("languageCollection",this.state.languageCollection);
-      console.log("languageURlCollection",this.state.languageURlCollection);
+    this.setState(
+      {
+        languageCollection: langauageArray,
+        languageURlCollection: languageURLArray,
+      },
+      () => {
+        // console.log("languageCollection",this.state.languageCollection);
+        // console.log("languageURlCollection",this.state.languageURlCollection);
+        this.getDeepLanguagePool();
+      }
+    );
+  };
+
+  getDeepLanguagePool = () => {
+    console.log('getDeepLanguagePool');
+    for (i = 0; i < this.state.languageURlCollection.length; i++) {
+      try {
+        fetch(this.state.languageURlCollection[i], {
+          method: 'GET',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+        })
+          .then(response => response.json())
+          .then(responseJson => {
+            console.log(responseJson);
+            this.setState(
+              {
+                repoLanguageResponse: responseJson,
+              },
+              () => {
+                this.poolLanguagesFromRepos();
+              }
+            );
+          });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  poolLanguagesFromRepos = () => {
+    var dictionary = [];
+
+    console.log('cg', this.state.repoLanguageResponse);
+
+    for (i=0;i<=this.state.repoLanguageResponse.length;i++){
+
+      dictionary.push({
+        key: Object.keys(ahash)[i],
+        value: this.state.repoLanguageResponse[Object.keys(ahash)[i]],
+      });
+
+    }
+
+    dictionary.push({
+      key: 'keyName',
+      value: 'the value',
     });
 
+    this.setState(
+      {
+        deepLanguageCollection: dictionary,
+      },
+      () => {
+        // console.log('hola', this.state.deepLanguageCollection);
+      }
+    );
   };
 
   render() {
